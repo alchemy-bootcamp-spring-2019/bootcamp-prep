8462be98e7858b92bbff1d45055710c398f82325
Paige Gorry <paigeegorry@gmail.com>
2019-04-09 15:12:10 -0700

    code cooking scripts for arrays, objects, variables, and numbers/strings/booleans

---
+++ class02-tdd-functions-1/introToVariables.js
@@ -0,0 +1,45 @@
+// Intro to variables
+
+   const numberFour = 4;
+
+   numberFour;
+
+   const helloWorld = 'helloWorld';
+
+   helloWorld;
+
+   const truthyThing = true;
+
+   truthyThing;
+
+// Reassigning Variables
+
+   let numChanger = 3;
+
+   numChanger;
+
+   numChanger = 4;
+
+   numChanger;
+
+   const strChanger = 'hello banana';
+   
+   strChanger;
+
+   strChanger = 'hello world';
+   
+   strChanger;
+
+   const boolChanger = true;
+
+   boolChanger;
+
+   boolChanger = false;
+
+   boolChanger;
+
+// Naming variables
+
+   const 123 = '123';
+
+   const 1Thing = 1;
