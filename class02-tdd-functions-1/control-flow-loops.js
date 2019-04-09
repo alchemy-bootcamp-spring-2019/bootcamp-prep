ec95d28eb09707c2b88b3f6269ba01e78f680bc3
Kate Dameron <katerj33@gmail.com>
2019-04-09 15:15:52 -0700

    js file for code cooking while and for loops with increment/decrement warm up

---
+++ class02-tdd-functions-1/control-flow-loops.js
@@ -0,0 +1,33 @@
+let count = 0;
+
+count;
+
+count += 1;
+
+count;
+
+count++;
+
+count;
+
+count -= 2;
+
+count;
+
+while (count < 5) {
+  console.log('The while loop is on count ' + count);
+  count++;
+}
+
+count;
+
+while (count < 10) {
+  console.log('The while loop is on count ' + count);
+  count++; 
+}
+
+count;
+
+for (let thisCount = 0; thisCount < 10; thisCount++) {
+  console.log("The for loop is on thisCount " +  thisCount);
+}
